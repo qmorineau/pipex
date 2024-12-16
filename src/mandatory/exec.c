39944ff5f4fc717a1cc1011c090302a8aa8074aa
qmorineau <q.morineau@gmail.com>
2024-12-16 16:51:00 +0100

    test

--- src/mandatory/exec.c
+++ src/mandatory/exec.c
@@ -1,34 +1,31 @@
 #include "pipex.h"
 
-
+void execve_error(void)
+{
+	perror("execve failed");
+	exit(EXIT_FAILURE);
+}
 void test_exec(char* cmd, char *envp[])
 {
 	char **args;
 	char *path;
 
 	args = ft_split(cmd, 32);
+	//free_tab(&args);
 	if (!args)
-		perror("split not work");
+		return ;
 	if (!access(cmd, X_OK))
 	{
 		if (execve(cmd, args, NULL) == -1)
-		{
-			perror("execve failed");
-			exit(EXIT_FAILURE);
-		}
+			execve_error();
 	}
 	path = find_path(args[0], envp);
 	if (!path)
 	{
 		if (execve(args[0], args, NULL) == -1)
-		{
-			perror("execve failed");
-			exit(EXIT_FAILURE);
-		}
+			execve_error();
 	}
 	if (execve(path, args, NULL) == -1)
-	{
-		perror("execve failed");
-		exit(EXIT_FAILURE);
-	}
+		execve_error();
 }
+
